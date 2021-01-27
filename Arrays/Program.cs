cfb273ff507095510f239dc1b6a7f3ce1e5c2dd7
Batuhan şen <batuhansen.035@gmail.com>
2021-01-27 04:55:20 +0300

    Add project files.

---
+++ Arrays/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] students = new string[3];
+            students[0] = "Engin";
+            students[1] = "Batuhan";
+            students[2] = "Büşra";
+
+            string[] students2 = { "Engin", "Batuhan", "Büşra" };
+
+            foreach (var student in students2)
+            {
+                Console.WriteLine(student );
+            }
+        }
+    }
+}
